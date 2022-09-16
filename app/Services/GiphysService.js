94325d73957204650aa315a96d603e6063b72bcc
Tung Le <tung.le0319@gmail.com>
2022-09-15 20:10:32 -0600

    changed getGiphy GET

--- app/Services/GiphysService.js
+++ app/Services/GiphysService.js
@@ -1,28 +1,38 @@
-import { appState } from "../AppState.js";
-import { Giphy } from "../Models/Giphy.js";
+import { appState } from '../AppState.js';
+import { Giphy } from '../Models/Giphy.js';
 import { GiphyApi } from './AxiosService.js';
 
 class GiphysService {
   fillForm(id) {
-    let tag = document.getElementById('tag')
-    let url = document.getElementById('url')
-    let gif = appState.giphys.find(g => g.id == id)
+    let tag = document.getElementById('tag');
+    let url = document.getElementById('url');
+    let gif = appState.giphys.find((g) => g.id == id);
     // @ts-ignore
-    tag.value = gif.tag
+    tag.value = gif.tag;
     // @ts-ignore
-    url.value = gif.url
+    url.value = gif.url;
   }
-  
-  async getGiphys(search) {
-  
-    const res = await GiphyApi.get(
-      `/search?api_key=SHAhnnBWk8sW7Pyga6hif9bQ8LLrlDj3&q=${search}&limit=25&offset=0&rating=pg-13&lang=en`);
 
-      console.log(res.data.data);
-      appState.giphys = res.data.data.map(gif=> new Giphy(gif))
-      console.log(appState.giphys)
-     
+  async getGiphys(search) {
+    const res = await GiphyApi.get(`/search`, {
+      params: {
+        api_key: 'SHAhnnBWk8sW7Pyga6hif9bQ8LLrlDj3',
+        limit: 50,
+        offset: 0,
+        rating: 'pg-13',
+        lang: 'en',
+        q: search
+      },
+    });
+
+    console.log(res.data.data);
+    appState.giphys = res.data.data.map((gif) => new Giphy(gif));
+    console.log(appState.giphys);
   }
 }
 
 export const giphysService = new GiphysService();
+
+
+// STUB the long query version 
+// /search?api_key=SHAhnnBWk8sW7Pyga6hif9bQ8LLrlDj3&q=${search}&limit=25&offset=0&rating=pg-13&lang=en
